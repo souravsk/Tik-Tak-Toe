7eae6d1fa3ffc50a6bea16b9373f1b734192314e
Sourav Kumar <souravk326@gmail.com>
2022-04-20 13:08:24 +0530

    full funcating

--- tiktakto.java
+++ tiktakto.java
@@ -1,9 +1,13 @@
 package project;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class tiktakto {
+
+    static ArrayList<Integer> playerposs = new ArrayList<>();
+    static ArrayList<Integer> cpuposs = new ArrayList<>();
+
     public static void main(String[] args) {
 
         char[][] gameboard = {{' ', '|', ' ', '|', ' '},
@@ -16,14 +20,34 @@ public class tiktakto {
         while (true) {
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter Your Placement (1-9)");
-            int pos = scan.nextInt();
-            placepiece(gameboard, pos, "Player");
+            int playerpos = scan.nextInt();
+            while(playerposs.contains(playerpos) || cpuposs.contains(playerpos)) {
+                System.out.println("Postion Taken");
+                playerpos = scan.nextInt();
+            }
+            placepiece(gameboard, playerpos, "Player");
+            String result = checkwin();
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
 
             Random ran = new Random();
             int cpupos = ran.nextInt(9) + 1;
+
+            while(playerposs.contains(cpupos) || cpuposs.contains(cpupos)) {
+                cpupos = ran.nextInt(9) + 1;
+            }
+
             placepiece(gameboard, cpupos, "CPU");
 
             printgameboard(gameboard);
+
+           result = checkwin();
+           if(result.length() > 0) {
+               System.out.println(result);
+               break;
+           }
 
         }
     }
@@ -42,9 +66,11 @@ public class tiktakto {
         char sysbol = ' ';
             if(user.equals("Player")){
                     sysbol = 'X';
+                    playerposs.add(pos);
                 }
             else if(user.equals("CPU")){
                 sysbol = '0';
+                cpuposs.add(pos);
             }
                 switch(pos){
                     case 1:
@@ -76,7 +102,42 @@ public class tiktakto {
                         break;
                     default:
                         break;
-                        
+
                 }
             }
-        }
+
+            public static String checkwin(){
+                List toprow = Arrays.asList(1, 2, 3);
+                List midrow = Arrays.asList(4, 5, 6);
+                List botrow = Arrays.asList(7, 8, 9);
+                List lefcol = Arrays.asList(1, 4, 7);
+                List midcol = Arrays.asList(2, 5, 8);
+                List rigcol = Arrays.asList(3, 6, 9);
+                List cross1 = Arrays.asList(3, 5, 7);
+                List cross2 = Arrays.asList(1, 5, 9);
+
+                List<List> winning = new ArrayList<>();
+                winning.add(toprow);
+                winning.add(midrow);
+                winning.add(botrow);
+                winning.add(lefcol);
+                winning.add(midcol);
+                winning.add(rigcol);
+                winning.add(cross1);
+                winning.add(cross2);
+
+                    for(List l : winning){
+                        if(playerposs.containsAll(l)) {
+                            return "You WIN";
+                        }
+                        else if(cpuposs.containsAll(l)) {
+                            return "Sorry CPU Win";
+                        }
+                        else if (playerposs.size() + cpuposs.size() == 9){
+                            return "CAT";
+                            }
+                        }
+                return "";
+                    }
+            }
+
